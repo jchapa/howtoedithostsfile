229834a2a1d9b42b1b110b0026d5b28fb4c25c64
Jack Chapa <jack@jackchapa.com>
2016-07-05 01:03:51 +0000

    more travis

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        karma: {
+            unit: {
+                configFile: 'config/karma.conf.js',
+                background: true
+            }
+        },
+        watch: {
+            karma: {
+                files: ['src/**/*.js', 'test/unit/**/*.js'],
+                tasks: ['karma:unit:run']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.registerTask('devmode', ['karma:unit', 'watch']);
+    
+    // Add a new task for travis
+    grunt.registerTask('test', ['karma:travis'])
+};
---
+++ test/unit/hostFileViewerSpec.js
@@ -0,0 +1,5 @@
+describe('version', function() {
+  it('should return current version', function {
+    expect('0.1').toEqual('0.1');
+  }));
+});
